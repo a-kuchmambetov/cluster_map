8bc609a2f30ac2dd3b23954037b43dc44117e24a
Artem Kuchmambetov <artem@kuchmambetov.dev>
2026-07-10 11:05:56 +0300

    fix: update production URL in docusaurus configuration

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
     },
 
     // Set the production url of your site here
-    url: "https://map-hive.pp.ua",
+    url: "https://docs.map-hive.pp.ua",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: "/",
